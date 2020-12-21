96d89559339bf05d7924187b0f3ae304e46ad3b4
MaikolCode <maikol320434@gmail.com>
2020-12-21 18:31:21 -0500

    se actualizo el home,se agregaron mas opciones

--- vista/moduloVista/home.php
+++ vista/moduloVista/home.php
@@ -1,8 +1,12 @@
 <?php
 session_start();
-    if(!isset($_SESSION['nombre'])){
-        header('location: ../../index.php');
-    }
+if (!isset($_SESSION['nombre'])) {
+    header('location: ../../index.php');
+}
+    include '../../controlador/conectionDB.php';
+
+    $sentencia = $conexion->query("SELECT * FROM farmbalance.animal;");
+    $listaAnimales = $sentencia->fetchAll(PDO::FETCH_OBJ);
 ?>
 <!DOCTYPE html>
 <html>
@@ -11,14 +15,140 @@ session_start();
         <title>Home</title>
         <link rel="stylesheet" href="../../multimedia/style.css"/> 
         <link rel="icon" href="../../multimedia/img/primerLogo.svg" type="image/png"/> 
+        <style>
+            .bienvenida,
+            .opciones-modulos{
+                width: 100%;
+                display: flex;
+                flex-direction: column;
+                align-items: center;
+                justify-content: center;
+            }
+            .contenido-textos{
+                width: 80%;
+                display: flex;
+                flex-direction: column;
+                align-items: center;
+                justify-content: center;
+            }
+            .contenedor-modulos{
+                width: 100%;
+                display: flex;
+                align-items: center;
+                justify-content: center;
+                margin-top: 50px;
+            }
+            .txt{
+                margin: 25px 0;
+                font-size: 20px;
+                font-weight: 300;
+                text-align: center;
+            }
+            .seccion-uno{
+                background-color: rgba(255, 255, 46, 0.4);
+                display: flex;
+                width: 100%;
+            }
+            .seccion,
+            .modulo-consulta{
+                display: flex;
+                flex-direction: column;
+            }
+            .seccion{
+                justify-content: flex-start;
+                width: 22%;
+                padding: 15px;
+                background-color: rgba(255, 255, 46, 0.9);
+                border-radius: 0 40px 40px 0;
+            }
+            .modulo-consulta{
+                width: 78%;
+                margin-left: 40px;
+                padding: 20px;
+            }
+            .ficha-animal{
+                display: flex;
+                flex-direction: row;
+            }
+            .cont-img{
+                display: flex;
+                justify-content: center;
+                width: 50%;
+                border-radius: 200px;
+                margin: 10px;
+                background-color: white;
+            }
+            .botones-mod{
+                display: flex;
+                flex-direction: column;
+            }
+            .ficha-animal{
+                display: flex;
+                margin-top: 35px;
+                align-items: center;
+            }
+            .animales-funcion{
+                width: 100%;
+            }
+            .accion{
+                display: flex;
+                align-items: center;
+                width: 60%;
+            }
+
+        </style>
     </head>
     <body>  
         <?php
-            include('../template/header.php');
+        include('../template/header.php');
         ?>
-        <h1>hola mundo</h1>
+
+        <div class="bienvenida"> 
+            <div class="contenido-textos">
+                <h2 style="margin: 55px 20px 5px 20px; padding: 0">Bienvenido a Farm Balance</h2>
+                <div class="separador"></div>
+                <p class="txt">Desde ahora sabras cuanto animales debes de comprar para conseguir ganancias a futuro, ademas de aprender un poco mas de los animales y de su cuidado, te daremos la orientación segun el animal que escojas, claramente cuidando tu bolsillo. <br><br> ¿Que esperas para comenzar?</p>
+            </div>
+        </div>
+        <div class="parallax-container" style="height: 200px">
+            <div class="parallax"><img src="../../multimedia/img/img-parallax.jpg" style="width:100%;"></div>
+        </div>
+        <div class="opciones-modulos">
+            <div class="contenedor-modulos">
+                <div class="seccion-uno">
+                    <div class="seccion">
+                        <span class="cont-img">
+                            <img src="../../multimedia/img/primerLogo.svg">
+                        </span>
+                        <div class="botones-mod">
+                            <p style="font-size: 20px; font-weight: 400; margin: 20px 0 0 0 ">¡Orientate conociendo nuestra granja!</p>
+                            <div class="separador" style="width: 90%;"></div>
+                            <a href="modulo1.php" style="margin: 20px 0" >Consultar Animales</a>
+                            <a href="modulo1.php" style="margin: 20px 0">Consultar Productos</a>
+                        </div>
+                    </div>
+                    <div class="modulo-consulta">
+                        <h3>Comencemos con cuantos y cuales animales</h3>
+                        <div class="animales-funcion">
+                            <?php foreach ($listaAnimales as $animal){?>
+                            <div class="ficha-animal">
+                                <img src="../../multimedia/img/<?=$animal->icono_animal?>" style="height:100px; width: 100px; margin: 0 20px; border-radius: 300px">
+                                <div style="margin: 0 30px"><p style="font-size: 30px; font-weight: 500"><?= $animal->nombreAnimal ?></p></div>
+                                <form class="accion">
+                                    <p style="margin-right: 10px">Cuantos animales?</p>
+                                    <input style="width: 15%" type="number" name="eleccion">
+                                    <a class="waves-effect waves-light btn" style="width: 19%;margin-left:10px">Calcular</a>
+                                </form>
+                            </div>
+                            <?php }?>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
         <?php
-            include('../template/footer.php');
+        include('../template/footer.php');
         ?>
     </body>
 </html>
--- vista/moduloVista/modulo1.php
+++ vista/moduloVista/modulo1.php
@@ -10,23 +10,47 @@ if (!isset($_SESSION['nombre'])) {
     $listaAnimales = $sentencia->fetchAll(PDO::FETCH_OBJ);
 }
 ?>
-<section id="portfolio">
-    <div class="container wow fadeInUp"> 
+<head>
+    <style>
+        .contenedor-tarjetas{
+            display: grid;
+            grid-template-columns: repeat(3, 1fr);
+            justify-content: center;
+        }
+        .tarjetas-animal{
+            margin: 10px;
+            width: 90%;
+        }
+        .seccion-animales{
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            width: 100%;
+        }
+        .contenedor-animales{
+            width: 95%;
+        }
+        
+    </style>
+</head>
+<section class="seccion-animales" id="portfolio">
+    <div class="contenedor-animales">
         <div class="row">
             <div class="col-md-12">
                 <h2><?php echo $_SESSION['nombre'] ?> estos son los animales que ofrecemos</h2>
             </div>
         </div>
-        <div class="row">
+        <div class="contenedor-tarjetas">
             <?php foreach ($listaAnimales as $animal) { ?>
-                <div class="col-md-3">
+                <div class="tarjetas-animal">
                     <a class="portfolio-item" style="background-image: url(../../multimedia/img/<?= $animal->icono_animal ?>)" href="">
                         <div class="details">
                             <h4><?= $animal->nombreAnimal ?></h4>
                             <span><?= $animal->pesoAnimal ?> Kg</span>
                             <span>$ <?= $animal->pesoVenta ?></span>
                             <span>Quedan: <?= $animal->cantidadAnimal . " " . $animal->nombreAnimal ?>s </span>
-                            <span>Ganancias: $ <?= ($animal->pesoVenta * $animal->cantidadAnimal)?> </span>
+                            <span>Ganancias: $ <?= ($animal->pesoVenta * $animal->cantidadAnimal) ?> </span>
                         </div>
                     </a>
                 </div>
